d8e87599fc1072d0e96b21e2c21611282d5e2af3
Fabio Henrique Minks Goncalves <fminks-g@c1r6s5.42lisboa.com>
2026-04-07 18:13:13 +0100

    Quarta_Vez

--- ex18/ft_print_params.c
+++ ex18/ft_print_params.c
@@ -1,13 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fminks-g <fminks-g@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/07 18:03:05 by fminks-g          #+#    #+#             */
+/*   Updated: 2026/04/07 18:03:30 by fminks-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void	ft_putstr(char *str)
-{
-	int	i;
+void	ft_putchar(char c);
 
-	i = 0;
-	while (str[i] != '\0')
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 && (*s1 == *s2))
 	{
-		write(1, &str[i], 1);
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+void	ft_sort_params(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	*tmp;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+			{
+				tmp = argv[i];
+				argv[i] = argv[j];
+				argv[j] = tmp;
+			}
+			j++;
+		}
 		i++;
 	}
 }
@@ -15,12 +51,18 @@ void	ft_putstr(char *str)
 int	main(int argc, char **argv)
 {
 	int	i;
+	int	j;
 
+	if (argc < 2)
+		return (0);
+	ft_sort_params(argc, argv);
 	i = 1;
 	while (i < argc)
 	{
-		ft_putstr(argv[i]);
-		write(1, "\n", 1);
+		j = 0;
+		while (argv[i][j])
+			ft_putchar(argv[i][j++]);
+		ft_putchar('\n');
 		i++;
 	}
 	return (0);
--- ex19/ft_sort_params.c
+++ ex19/ft_sort_params.c
@@ -1,36 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fminks-g <fminks-g@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/07 17:53:23 by fminks-g          #+#    #+#             */
+/*   Updated: 2026/04/07 18:01:31 by fminks-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void	ft_putstr(char *str)
-{
-	int	i;
+#include <unistd.h>
 
-	i = 0;
-	while (str[i])
-	{
-		write(1, &str[i], 1);
-		i++;
-	}
-	write(1, "\n", 1);
-}
+void	ft_putchar(char c);
 
 int	ft_strcmp(char *s1, char *s2)
 {
-	int	i;
-
-	i = 0;
-	while (s1[i] && s1[i] == s2[i])
-		i++;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
 
-int	main(int argc, char **argv)
+void	ft_ordem_argv(int argc, char **argv)
 {
 	int		i;
 	int		j;
 	char	*tmp;
 
-	if (argc < 2)
-		return (0);
 	i = 1;
 	while (i < argc - 1)
 	{
@@ -47,8 +48,27 @@ int	main(int argc, char **argv)
 		}
 		i++;
 	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	if (argc < 2)
+		return (0);
+	ft_ordem_argv(argc, argv);
 	i = 1;
 	while (i < argc)
-		ft_putstr(argv[i++]);
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			ft_putchar(argv[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
 	return (0);
 }
